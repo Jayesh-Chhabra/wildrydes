88d0c8dba649aefd2cc05820e10b823a4673f758
Jayesh-Chhabra <97704286+Jayesh-Chhabra@users.noreply.github.com>
2022-07-05 18:58:40 -0700

    final update hopefully

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://eowjx1lcme.execute-api.us-west-1.amazonaws.com/ride/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://eowjx1lcme.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
