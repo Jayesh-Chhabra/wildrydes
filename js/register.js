ab451659394d9bb3bba79232ac9c1767e2499abc
Jayesh-Chhabra <97704286+Jayesh-Chhabra@users.noreply.github.com>
2022-07-08 13:01:43 -0700

    d

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@ let button = document.getElementById("login");
 button.onclick = function() {
     window.location.href = "/signin.html";
   }
-let button2 = document.getElementById("home");
-button2.onclick = function() {
+let button35 = document.getElementById("home1231");
+button35.onclick = function() {
     window.location.href = "/ride.html";
   }
