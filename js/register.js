1c721c25b1ebcf2eea9da80a0a196389cb1e9346
Jayesh-Chhabra <97704286+Jayesh-Chhabra@users.noreply.github.com>
2022-07-08 12:12:44 -0700

    getting the hang of this

--- js/register.js
+++ js/register.js
@@ -2,3 +2,7 @@ let button = document.getElementById("login");
 button.onclick = function() {
     window.location.href = "/signin.html";
   }
+  let button = document.getElementById("home");
+button.onclick = function() {
+    window.location.href = "/";
+  }
