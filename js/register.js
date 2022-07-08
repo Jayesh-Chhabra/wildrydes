c5f51848fe2652d4f93175873815a4ba2922db3e
Jayesh-Chhabra <97704286+Jayesh-Chhabra@users.noreply.github.com>
2022-07-08 13:13:25 -0700

    final one for real

--- js/register.js
+++ js/register.js
@@ -2,7 +2,3 @@ let button = document.getElementById("login");
 button.onclick = function() {
     window.location.href = "/signin.html";
   }
-let button35 = document.getElementById("home1231");
-button35.onclick = function() {
-    window.location.href = "/ride.html";
-  }
---
+++ js/signin.js
@@ -0,0 +1,4 @@
+let button35 = document.getElementById("home");
+button35.onclick = function() {
+    window.location.href = "/index.html";
+  }
